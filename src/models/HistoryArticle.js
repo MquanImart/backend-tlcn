8bb8de5f676d582cb55542892a16529de32b75d0
MquanImart <phanminhquanmh@gmail.com>
2025-04-25 22:51:11 +0700

    Thêm model và tạo AI

--- src/models/HistoryArticle.js
+++ src/models/HistoryArticle.js
@@ -3,6 +3,7 @@ import mongoose, { Schema, Document } from 'mongoose';
 const HistoryArticleSchema = new Schema({
   idUser: { type: String, required: true },
   idArticle: { type: String, required: true },
+  action: { type: "View" | "Like", default: "View"},
   viewDate: { type: Number, default: () => Date.now() },
 });
 
