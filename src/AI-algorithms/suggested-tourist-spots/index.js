26d53a0ff466b822495653eb9df71a5dc0654a11
MquanImart <phanminhquanmh@gmail.com>
2025-06-02 16:25:34 +0700

    không đẩy file json gg

--- src/AI-algorithms/suggested-tourist-spots/index.js
+++ src/AI-algorithms/suggested-tourist-spots/index.js
@@ -28,7 +28,7 @@ const suggestedPageMonth = async (req, res) => {
     const result = await suggestedPageService.suggestedPageMonth(req.params.id, req.query.month);
     if (!result.success) return res.status(400).json({ success: false, message: result.message });
     
-    res.status(200).json({ success: true, data: result.data, message: 'Danh sách gợi ý địa điểm du lịch cho người dùng' })
+    res.status(200).json({ success: true, data: result.data.data, message: 'Danh sách gợi ý địa điểm du lịch cho người dùng' })
   } catch (error) {
     res.status(500).json({ success: false, data: null, message: error.message })
   }
